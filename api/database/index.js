f3f718e506657db5d019185b6356c10031eedb05
Erwan Vallée <erwanp.vallee@gmail.com>
2026-08-17 11:28:57 +0200

    Lit la chaîne de connexion Mongo dans une variable d'environnement

--- api/database/index.js
+++ api/database/index.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
+// Une chaîne de connexion contient un identifiant et un mot de passe : elle se
+// lit dans une variable d'environnement, elle ne s'écrit pas dans le code.
+const MONGO_URL =
+  process.env.MONGO_URL ??
+  "mongodb+srv://UTILISATEUR:MOT_DE_PASSE@CLUSTER.mongodb.net/r18jwt?retryWrites=true&w=majority";
+
 mongoose
-  .connect(
-    "mongodb+srv://jean:123@cluster0.urpjt.gcp.mongodb.net/r18jwt?retryWrites=true&w=majority"
-  )
+  .connect(MONGO_URL)
   .then(() => {
     console.log("CONNEXION DB OK !");
   })
